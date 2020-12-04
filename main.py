dd38a2799bfb6a0791fdec268edba203d3728f86
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2020-12-03 22:30:24 -0700

    Reorganized to seperate concerns

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import fastapi
 import uvicorn
-from starlette.requests import Request
 from starlette.staticfiles import StaticFiles
-from starlette.templating import Jinja2Templates
+
+from api import weather_api
+from views import home
 
 api = fastapi.FastAPI()
-templates = Jinja2Templates('templates')
-api.mount('/static', StaticFiles(directory='static'), name='static')
 
 
-@api.get('/')
-def index(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request})
+def configure_routing():
+    api.mount('/static', StaticFiles(directory='static'), name='static')
+    api.include_router(home.router)
+    api.include_router(weather_api.router)
+
+
+def configure():
+    configure_routing()
 
 
 if __name__ == "__main__":
+    configure()
     uvicorn.run(api, port=8080, host='127.0.0.1')
+else:
+    configure()
