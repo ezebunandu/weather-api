2b46ad09b9e126523cf2e677a997f2a42426a32f
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2020-12-03 20:08:52 -0700

    Home page with templates

---
+++ main.py
@@ -0,0 +1,18 @@
+import fastapi
+import uvicorn
+from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
+
+api = fastapi.FastAPI()
+templates = Jinja2Templates('templates')
+api.mount('/static', StaticFiles(directory='static'), name='static')
+
+
+@api.get('/')
+def index(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+
+if __name__ == "__main__":
+    uvicorn.run(api, port=8080, host='127.0.0.1')
