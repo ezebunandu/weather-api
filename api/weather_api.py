1993138a8e15cacb5b7bc7f57c8447f1e7ac219c
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2020-12-16 05:00:02 -0700

    Calling the open weather api backend async

--- api/weather_api.py
+++ api/weather_api.py
@@ -9,6 +9,6 @@ router = fastapi.APIRouter()
 
 
 @router.get('/api/weather/{city}')
-def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
-    report = open_weather_service.get_report(loc.city, loc.state, loc.country, units)
+async def weather(loc: Location = Depends(), units: Optional[str] = 'metric'):
+    report = await open_weather_service.get_report(loc.city, loc.state, loc.country, units)
     return report
--- services/open_weather_service.py
+++ services/open_weather_service.py
@@ -1,22 +1,23 @@
 from typing import Optional
 import dotenv
 import os
-import requests
+import httpx
 
 dotenv.load_dotenv()
-API_KEY = os.getenv('OPEN_WEATHER_KEY')
+api_key = os.getenv('OPEN_WEATHER_KEY')
 
 
-def get_report(city: str, state: Optional[str], country: str, units: str) -> dict:
+async def get_report(city: str, state: Optional[str], country: str, units: str) -> dict:
     if state:
         q = f'{city},{state},{country}'
     else:
         q = f'{city},{country}'
-    api_key = API_KEY
+
     url = f"https://api.openweathermap.org/data/2.5/weather?q={q}&appid={api_key}&units={units}"
 
-    resp = requests.get(url)
-    resp.raise_for_status()
+    async with httpx.AsyncClient() as client:
+        resp = await client.get(url)
+        resp.raise_for_status()
 
     data = resp.json()
     return data["main"]
